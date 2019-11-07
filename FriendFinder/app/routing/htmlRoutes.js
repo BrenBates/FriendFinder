8b5679a7c12484b4cbac73fe2ec12b370f59741b
Bren <brennenbates2859@gmail.com>
2019-11-07 00:08:50 -0700

    Initial file creations
    initial file creations

---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = function(app) { 
+   
+//Set up a default, catch-all route that leads to home.html
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/home.html"));
+});
+
+//Set up a get route to /survey to display the survey page. 
+app.get("/survey", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/survey.html"));
+});
+
+
+}
+
